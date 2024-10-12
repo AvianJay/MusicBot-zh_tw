807f60168305345030d6ab601f8f2cc3f3ede583
AvianJay <48914361+AvianJay@users.noreply.github.com>
2024-10-12 21:47:41 +0800

    Translate

--- src/main/java/com/jagrosh/jmusicbot/commands/admin/SetdjCmd.java
+++ src/main/java/com/jagrosh/jmusicbot/commands/admin/SetdjCmd.java
@@ -1,17 +1,16 @@
 /*
- * Copyright 2018 John Grosh <john.a.grosh@gmail.com>.
+ * 版權所有 2018 John Grosh <john.a.grosh@gmail.com>.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * 根據 Apache 許可證 2.0 版（"許可證"）授權；
+ * 除非遵守許可證，否則你不能使用此檔案。
+ * 你可以在以下網址獲得許可證副本：
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * 除非適用的法律要求或書面同意，
+ * 根據許可證分發的軟體是在 "原樣" 基礎上提供的，
+ * 不附帶任何形式的明示或默示擔保或條件。
+ * 有關許可證下具體語言的權限和限制，請參見許可證。
  */
 package com.jagrosh.jmusicbot.commands.admin;
 
@@ -33,8 +32,8 @@ public class SetdjCmd extends AdminCommand
     public SetdjCmd(Bot bot)
     {
         this.name = "setdj";
-        this.help = "sets the DJ role for certain music commands";
-        this.arguments = "<rolename|NONE>";
+        this.help = "設置 DJ 角色來使用某些音樂命令";
+        this.arguments = "<角色名|NONE>";
         this.aliases = bot.getConfig().getAliases(this.name);
     }
     
@@ -43,28 +42,27 @@ public class SetdjCmd extends AdminCommand
     {
         if(event.getArgs().isEmpty())
         {
-            event.reply(event.getClient().getError()+" Please include a role name or NONE");
+            event.reply(event.getClient().getError()+" 請包含角色名或輸入 NONE");
             return;
         }
         Settings s = event.getClient().getSettingsFor(event.getGuild());
         if(event.getArgs().equalsIgnoreCase("none"))
         {
             s.setDJRole(null);
-            event.reply(event.getClient().getSuccess()+" DJ role cleared; Only Admins can use the DJ commands.");
+            event.reply(event.getClient().getSuccess()+" DJ 角色已清除；只有管理員可以使用 DJ 命令。");
         }
         else
         {
             List<Role> list = FinderUtil.findRoles(event.getArgs(), event.getGuild());
             if(list.isEmpty())
-                event.reply(event.getClient().getWarning()+" No Roles found matching \""+event.getArgs()+"\"");
+                event.reply(event.getClient().getWarning()+" 找不到匹配 \""+event.getArgs()+"\" 的角色");
             else if (list.size()>1)
                 event.reply(event.getClient().getWarning()+FormatUtil.listOfRoles(list, event.getArgs()));
             else
             {
                 s.setDJRole(list.get(0));
-                event.reply(event.getClient().getSuccess()+" DJ commands can now be used by users with the **"+list.get(0).getName()+"** role.");
+                event.reply(event.getClient().getSuccess()+" DJ 命令現在可以由具有 **"+list.get(0).getName()+"** 角色的用戶使用。");
             }
         }
     }
-    
 }
