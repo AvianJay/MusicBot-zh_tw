679dbe59b2a3f73b79cb14089b1f68e7f13d0a78
AvianJay <48914361+AvianJay@users.noreply.github.com>
2024-10-12 21:56:09 +0800

    Translate

--- src/main/java/com/jagrosh/jmusicbot/commands/dj/MoveTrackCmd.java
+++ src/main/java/com/jagrosh/jmusicbot/commands/dj/MoveTrackCmd.java
@@ -1,6 +1,5 @@
 package com.jagrosh.jmusicbot.commands.dj;
 
-
 import com.jagrosh.jdautilities.command.CommandEvent;
 import com.jagrosh.jmusicbot.Bot;
 import com.jagrosh.jmusicbot.audio.AudioHandler;
@@ -9,16 +8,15 @@ import com.jagrosh.jmusicbot.commands.DJCommand;
 import com.jagrosh.jmusicbot.queue.AbstractQueue;
 
 /**
- * Command that provides users the ability to move a track in the playlist.
+ * 提供用戶移動播放列表中曲目的能力的命令。
  */
 public class MoveTrackCmd extends DJCommand
 {
-
     public MoveTrackCmd(Bot bot)
     {
         super(bot);
         this.name = "movetrack";
-        this.help = "move a track in the current queue to a different position";
+        this.help = "將當前隊列中的曲目移動到不同位置";
         this.arguments = "<from> <to>";
         this.aliases = bot.getConfig().getAliases(this.name);
         this.bePlaying = true;
@@ -33,48 +31,48 @@ public class MoveTrackCmd extends DJCommand
         String[] parts = event.getArgs().split("\\s+", 2);
         if(parts.length < 2)
         {
-            event.replyError("Please include two valid indexes.");
+            event.replyError("請提供兩個有效的索引。");
             return;
         }
 
         try
         {
-            // Validate the args
+            // 驗證參數
             from = Integer.parseInt(parts[0]);
             to = Integer.parseInt(parts[1]);
         }
         catch (NumberFormatException e)
         {
-            event.replyError("Please provide two valid indexes.");
+            event.replyError("請提供兩個有效的索引。");
             return;
         }
 
         if (from == to)
         {
-            event.replyError("Can't move a track to the same position.");
+            event.replyError("無法將曲目移到相同位置。");
             return;
         }
 
-        // Validate that from and to are available
+        // 驗證 from 和 to 是否有效
         AudioHandler handler = (AudioHandler) event.getGuild().getAudioManager().getSendingHandler();
         AbstractQueue<QueuedTrack> queue = handler.getQueue();
         if (isUnavailablePosition(queue, from))
         {
-            String reply = String.format("`%d` is not a valid position in the queue!", from);
+            String reply = String.format("`%d` 不是隊列中的有效位置！", from);
             event.replyError(reply);
             return;
         }
         if (isUnavailablePosition(queue, to))
         {
-            String reply = String.format("`%d` is not a valid position in the queue!", to);
+            String reply = String.format("`%d` 不是隊列中的有效位置！", to);
             event.replyError(reply);
             return;
         }
 
-        // Move the track
+        // 移動曲目
         QueuedTrack track = queue.moveItem(from - 1, to - 1);
         String trackTitle = track.getTrack().getInfo().title;
-        String reply = String.format("Moved **%s** from position `%d` to `%d`.", trackTitle, from, to);
+        String reply = String.format("已將 **%s** 從位置 `%d` 移動到 `%d`。", trackTitle, from, to);
         event.replySuccess(reply);
     }
 
