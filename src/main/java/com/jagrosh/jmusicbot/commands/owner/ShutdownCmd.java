7a7e38f26f4d7843c5addf9606550b1210dbea56
AvianJay <48914361+AvianJay@users.noreply.github.com>
2024-10-14 10:40:02 +0800

    Translate

--- src/main/java/com/jagrosh/jmusicbot/commands/owner/ShutdownCmd.java
+++ src/main/java/com/jagrosh/jmusicbot/commands/owner/ShutdownCmd.java
@@ -1,17 +1,15 @@
 /*
  * Copyright 2017 John Grosh <john.a.grosh@gmail.com>.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * 根據 Apache License 2.0 版（以下簡稱「許可證」）授權使用本文件；
+ * 除非遵守許可證，否則您不得使用本文件。
+ * 您可以在以下網址獲取許可證副本：
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * 除非適用法律要求或書面同意，根據許可證分發的軟體按「現狀」提供，
+ * 不附帶任何明示或默示的保證或條件。
+ * 請參閱許可證以瞭解具體的許可權和限制。
  */
 package com.jagrosh.jmusicbot.commands.owner;
 
@@ -25,21 +23,21 @@ import com.jagrosh.jmusicbot.commands.OwnerCommand;
  */
 public class ShutdownCmd extends OwnerCommand
 {
-    private final Bot bot;
+    private final Bot bot; // 機器人實例
     
     public ShutdownCmd(Bot bot)
     {
         this.bot = bot;
-        this.name = "shutdown";
-        this.help = "safely shuts down";
-        this.aliases = bot.getConfig().getAliases(this.name);
-        this.guildOnly = false;
+        this.name = "shutdown"; // 指令名稱
+        this.help = "安全關閉"; // 指令說明
+        this.aliases = bot.getConfig().getAliases(this.name); // 別名
+        this.guildOnly = false; // 可以在私人頻道使用
     }
     
     @Override
     protected void execute(CommandEvent event)
     {
-        event.replyWarning("Shutting down...");
-        bot.shutdown();
+        event.replyWarning("正在關閉...");
+        bot.shutdown(); // 關閉機器人
     }
 }
