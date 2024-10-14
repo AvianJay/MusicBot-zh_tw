2bbd67e18d2ab075f43a4c38e4109228c67c1e63
AvianJay <48914361+AvianJay@users.noreply.github.com>
2024-10-14 13:06:05 +0800

    Retranslate

--- src/main/java/com/jagrosh/jmusicbot/commands/music/SearchCmd.java
+++ src/main/java/com/jagrosh/jmusicbot/commands/music/SearchCmd.java
@@ -1,15 +1,16 @@
 /*
- * Copyright 2016 John Grosh <john.a.grosh@gmail.com>.
+ * 版權所有 2016 John Grosh <john.a.grosh@gmail.com>。
  *
- * 根據 Apache License 2.0 版（以下簡稱「許可證」）授權使用本文件；
- * 除非遵守許可證，否則您不得使用本文件。
- * 您可以在以下網址獲取許可證副本：
+ * 根據 Apache 許可證 2.0 版（"許可證"）授權；
+ * 除非遵守許可證，否則你不能使用此檔案。
+ * 你可以在以下網址獲得許可證副本：
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * 除非適用法律要求或書面同意，根據許可證分發的軟體按「現狀」提供，
- * 不附帶任何明示或默示的保證或條件。
- * 請參閱許可證以瞭解具體的許可權和限制。
+ * 除非適用的法律要求或書面同意，
+ * 根據許可證分發的軟體是在 "原樣" 基礎上提供的，
+ * 不附帶任何形式的明示或默示擔保或條件。
+ * 有關許可證下具體語言的權限和限制，請參見許可證。
  */
 package com.jagrosh.jmusicbot.commands.music;
 
@@ -30,7 +31,6 @@ import com.jagrosh.jmusicbot.commands.MusicCommand;
 import com.jagrosh.jmusicbot.utils.FormatUtil;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.exceptions.PermissionException;
 
 /**
  *
@@ -38,40 +38,39 @@ import net.dv8tion.jda.api.exceptions.PermissionException;
  */
 public class SearchCmd extends MusicCommand 
 {
-    protected String searchPrefix = "ytsearch:"; // 搜尋前綴
+    protected String searchPrefix = "ytsearch:";
     private final OrderedMenu.Builder builder;
     private final String searchingEmoji;
     
     public SearchCmd(Bot bot)
     {
         super(bot);
-        this.searchingEmoji = bot.getConfig().getSearching(); // 獲取搜尋中的表情符號
-        this.name = "search"; // 指令名稱為 search
-        this.aliases = bot.getConfig().getAliases(this.name); // 指令的別名
-        this.arguments = "<查詢>"; // 指令參數
-        this.help = "搜尋 YouTube 上提供的查詢"; // 指令說明
-        this.beListening = true; // 需要正在收聽
-        this.bePlaying = false; // 不需要正在播放音樂
-        this.botPermissions = new Permission[]{Permission.MESSAGE_EMBED_LINKS}; // 機器人所需權限
+        this.searchingEmoji = bot.getConfig().getSearching();
+        this.name = "search";
+        this.aliases = bot.getConfig().getAliases(this.name);
+        this.arguments = "<query>";
+        this.help = "搜索提供的查詢的 YouTube";
+        this.beListening = true;
+        this.bePlaying = false;
+        this.botPermissions = new Permission[]{Permission.MESSAGE_EMBED_LINKS};
         builder = new OrderedMenu.Builder()
-                .allowTextInput(true) // 允許文字輸入
-                .useNumbers() // 使用編號選項
-                .useCancelButton(true) // 使用取消按鈕
-                .setEventWaiter(bot.getWaiter()) // 設定事件等待器
-                .setTimeout(1, TimeUnit.MINUTES); // 設定超時時間為1分鐘
+                .allowTextInput(true)
+                .useNumbers()
+                .useCancelButton(true)
+                .setEventWaiter(bot.getWaiter())
+                .setTimeout(1, TimeUnit.MINUTES);
     }
     
     @Override
     public void doCommand(CommandEvent event) 
     {
-        if(event.getArgs().isEmpty()) // 如果沒有提供查詢
+        if(event.getArgs().isEmpty())
         {
-            event.replyError("請包含一個查詢。"); // 回覆錯誤訊息
+            event.replyError("請包含一個查詢。");
             return;
         }
-        // 回覆搜尋中訊息，並開始載入項目
-        event.reply(searchingEmoji+" 搜尋中... `["+event.getArgs()+"]`", 
-                m -> bot.getPlayerManager().loadItemOrdered(event.getGuild(), searchPrefix + event.getArgs(), new ResultHandler(m,event)));
+        event.reply(searchingEmoji + " 正在搜尋... `[" + event.getArgs() + "]`", 
+                m -> bot.getPlayerManager().loadItemOrdered(event.getGuild(), searchPrefix + event.getArgs(), new ResultHandler(m, event)));
     }
     
     private class ResultHandler implements AudioLoadResultHandler 
@@ -88,63 +87,63 @@ public class SearchCmd extends MusicCommand
         @Override
         public void trackLoaded(AudioTrack track)
         {
-            if(bot.getConfig().isTooLong(track)) // 如果曲目太長
+            if(bot.getConfig().isTooLong(track))
             {
-                m.editMessage(FormatUtil.filter(event.getClient().getWarning()+" 此曲目 (**"+track.getInfo().title+"**) 超過允許的最大長度：`"
-                        + TimeUtil.formatTime(track.getDuration())+"` > `"+TimeUtil.formatTime(bot.getConfig().getMaxTime())+"`")).queue();
+                m.editMessage(FormatUtil.filter(event.getClient().getWarning() + " 此曲目 (**" + track.getInfo().title + "**) 超過允許的最大長度: `"
+                        + TimeUtil.formatTime(track.getDuration()) + "` > `" + bot.getConfig().getMaxTime() + "`")).queue();
                 return;
             }
-            AudioHandler handler = (AudioHandler)event.getGuild().getAudioManager().getSendingHandler(); // 獲取音訊處理器
-            int pos = handler.addTrack(new QueuedTrack(track, RequestMetadata.fromResultHandler(track, event)))+1; // 添加曲目到佇列
-            m.editMessage(FormatUtil.filter(event.getClient().getSuccess()+" 已添加 **"+track.getInfo().title
-                    +"** (`"+ TimeUtil.formatTime(track.getDuration())+"`) "+(pos==0 ? "開始播放"
-                        : " 到佇列中的位置 "+pos))).queue(); // 回覆成功訊息
+            AudioHandler handler = (AudioHandler)event.getGuild().getAudioManager().getSendingHandler();
+            int pos = handler.addTrack(new QueuedTrack(track, RequestMetadata.fromResultHandler(track, event))) + 1;
+            m.editMessage(FormatUtil.filter(event.getClient().getSuccess() + " 已將 **" + track.getInfo().title
+                    + "** (`" + TimeUtil.formatTime(track.getDuration()) + "`) " + (pos == 0 ? "設置為開始播放"
+                        : " 加入隊列在位置 " + pos))).queue();
         }
 
         @Override
         public void playlistLoaded(AudioPlaylist playlist)
         {
-            builder.setColor(event.getSelfMember().getColor()) // 設定顏色
-                    .setText(FormatUtil.filter(event.getClient().getSuccess()+" 查詢結果 `"+event.getArgs()+"`：")) // 設定文本
-                    .setChoices(new String[0]) // 清除選項
-                    .setSelection((msg,i) -> 
+            builder.setColor(event.getSelfMember().getColor())
+                    .setText(FormatUtil.filter(event.getClient().getSuccess() + " `"+event.getArgs()+"` 的搜索結果:"))
+                    .setChoices(new String[0])
+                    .setSelection((msg, i) -> 
                     {
-                        AudioTrack track = playlist.getTracks().get(i-1); // 獲取選擇的曲目
-                        if(bot.getConfig().isTooLong(track)) // 如果曲目太長
+                        AudioTrack track = playlist.getTracks().get(i - 1);
+                        if(bot.getConfig().isTooLong(track))
                         {
-                            event.replyWarning("此曲目 (**"+track.getInfo().title+"**) 超過允許的最大長度：`"
-                                    + TimeUtil.formatTime(track.getDuration())+"` > `"+bot.getConfig().getMaxTime()+"`");
+                            event.replyWarning("此曲目 (**" + track.getInfo().title + "**) 超過允許的最大長度: `"
+                                    + TimeUtil.formatTime(track.getDuration()) + "` > `" + bot.getConfig().getMaxTime() + "`");
                             return;
                         }
-                        AudioHandler handler = (AudioHandler)event.getGuild().getAudioManager().getSendingHandler(); // 獲取音訊處理器
-                        int pos = handler.addTrack(new QueuedTrack(track, RequestMetadata.fromResultHandler(track, event)))+1; // 添加曲目到佇列
-                        event.replySuccess("已添加 **" + FormatUtil.filter(track.getInfo().title)
-                                + "** (`" + TimeUtil.formatTime(track.getDuration()) + "`) " + (pos==0 ? "開始播放" 
-                                    : " 到佇列中的位置 "+pos)); // 回覆成功訊息
+                        AudioHandler handler = (AudioHandler)event.getGuild().getAudioManager().getSendingHandler();
+                        int pos = handler.addTrack(new QueuedTrack(track, RequestMetadata.fromResultHandler(track, event))) + 1;
+                        event.replySuccess("已將 **" + FormatUtil.filter(track.getInfo().title)
+                                + "** (`" + TimeUtil.formatTime(track.getDuration()) + "`) " + (pos == 0 ? "設置為開始播放" 
+                                    : " 加入隊列在位置 " + pos));
                     })
-                    .setCancel((msg) -> {}) // 設定取消動作
-                    .setUsers(event.getAuthor()); // 設定用戶
-            for(int i=0; i<4 && i<playlist.getTracks().size(); i++) // 迭代前4個曲目
+                    .setCancel((msg) -> {})
+                    .setUsers(event.getAuthor());
+            for(int i = 0; i < 4 && i < playlist.getTracks().size(); i++)
             {
                 AudioTrack track = playlist.getTracks().get(i);
-                builder.addChoices("`["+ TimeUtil.formatTime(track.getDuration())+"]` [**"+track.getInfo().title+"**]("+track.getInfo().uri+")"); // 添加選項
+                builder.addChoices("`[" + TimeUtil.formatTime(track.getDuration()) + "]` [**" + track.getInfo().title + "**](" + track.getInfo().uri + ")");
             }
-            builder.build().display(m); // 顯示選單
+            builder.build().display(m);
         }
 
         @Override
         public void noMatches() 
         {
-            m.editMessage(FormatUtil.filter(event.getClient().getWarning()+" 找不到 `"+event.getArgs()+"` 的結果。")).queue(); // 回覆沒有結果的訊息
+            m.editMessage(FormatUtil.filter(event.getClient().getWarning() + " 沒有找到 `"+event.getArgs()+"` 的結果。")).queue();
         }
 
         @Override
         public void loadFailed(FriendlyException throwable) 
         {
             if(throwable.severity == Severity.COMMON)
-                m.editMessage(event.getClient().getError()+" 載入錯誤："+throwable.getMessage()).queue(); // 回覆載入錯誤訊息
+                m.editMessage(event.getClient().getError() + " 加載錯誤: " + throwable.getMessage()).queue();
             else
-                m.editMessage(event.getClient().getError()+" 載入曲目時發生錯誤。").queue(); // 回覆通用載入錯誤訊息
+                m.editMessage(event.getClient().getError() + " 加載曲目時發生錯誤。").queue();
         }
     }
 }
