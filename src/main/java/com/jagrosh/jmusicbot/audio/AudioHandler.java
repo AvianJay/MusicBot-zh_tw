46eb343abdb58507f22b3e91445f16f8858dcc86
AvianJay <48914361+AvianJay@users.noreply.github.com>
2024-10-14 12:21:36 +0800

    Translate

--- src/main/java/com/jagrosh/jmusicbot/audio/AudioHandler.java
+++ src/main/java/com/jagrosh/jmusicbot/audio/AudioHandler.java
@@ -1,17 +1,15 @@
 /*
- * Copyright 2016 John Grosh <john.a.grosh@gmail.com>.
+ * 版權所有 2016 John Grosh <john.a.grosh@gmail.com>。
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * 根據 Apache 許可證 版本 2.0（以下簡稱“許可證”）授權；
+ * 除非遵守該許可證，否則您不得使用此檔案。
+ * 您可以在以下網址獲取許可證的副本：
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * 除非法律要求或書面同意，否則根據許可證分發的軟件是以“原樣”基礎提供的，
+ * 不附有任何明示或隱含的保證或條件。
+ * 有關授權的具體語言以及對於許可證的限制，請參見許可證。
  */
 package com.jagrosh.jmusicbot.audio;
 
@@ -80,7 +78,7 @@ public class AudioHandler extends AudioEventAdapter implements AudioSendHandler
 
     public int addTrackToFront(QueuedTrack qtrack)
     {
-        if(audioPlayer.getPlayingTrack()==null)
+        if(audioPlayer.getPlayingTrack() == null)
         {
             audioPlayer.playTrack(qtrack.getTrack());
             return -1;
@@ -94,7 +92,7 @@ public class AudioHandler extends AudioEventAdapter implements AudioSendHandler
     
     public int addTrack(QueuedTrack qtrack)
     {
-        if(audioPlayer.getPlayingTrack()==null)
+        if(audioPlayer.getPlayingTrack() == null)
         {
             audioPlayer.playTrack(qtrack.getTrack());
             return -1;
@@ -118,7 +116,7 @@ public class AudioHandler extends AudioEventAdapter implements AudioSendHandler
     
     public boolean isMusicPlaying(JDA jda)
     {
-        return guild(jda).getSelfMember().getVoiceState().inVoiceChannel() && audioPlayer.getPlayingTrack()!=null;
+        return guild(jda).getSelfMember().getVoiceState().inVoiceChannel() && audioPlayer.getPlayingTrack() != null;
     }
     
     public Set<String> getVotes()
@@ -147,15 +145,15 @@ public class AudioHandler extends AudioEventAdapter implements AudioSendHandler
             return true;
         }
         Settings settings = manager.getBot().getSettingsManager().getSettings(guildId);
-        if(settings==null || settings.getDefaultPlaylist()==null)
+        if(settings == null || settings.getDefaultPlaylist() == null)
             return false;
         
         Playlist pl = manager.getBot().getPlaylistLoader().getPlaylist(settings.getDefaultPlaylist());
-        if(pl==null || pl.getItems().isEmpty())
+        if(pl == null || pl.getItems().isEmpty())
             return false;
         pl.loadTracks(manager, (at) -> 
         {
-            if(audioPlayer.getPlayingTrack()==null)
+            if(audioPlayer.getPlayingTrack() == null)
                 audioPlayer.playTrack(at);
             else
                 defaultQueue.add(at);
@@ -167,13 +165,13 @@ public class AudioHandler extends AudioEventAdapter implements AudioSendHandler
         return true;
     }
     
-    // Audio Events
+    // 音頻事件
     @Override
     public void onTrackEnd(AudioPlayer player, AudioTrack track, AudioTrackEndReason endReason) 
     {
         RepeatMode repeatMode = manager.getBot().getSettingsManager().getSettings(guildId).getRepeatMode();
-        // if the track ended normally, and we're in repeat mode, re-add it to the queue
-        if(endReason==AudioTrackEndReason.FINISHED && repeatMode != RepeatMode.OFF)
+        // 如果曲目正常結束，且我們在重複模式中，則重新添加到隊列中
+        if(endReason == AudioTrackEndReason.FINISHED && repeatMode != RepeatMode.OFF)
         {
             QueuedTrack clone = new QueuedTrack(track.makeClone(), track.getUserData(RequestMetadata.class));
             if(repeatMode == RepeatMode.ALL)
@@ -189,8 +187,8 @@ public class AudioHandler extends AudioEventAdapter implements AudioSendHandler
                 manager.getBot().getNowplayingHandler().onTrackUpdate(null);
                 if(!manager.getBot().getConfig().getStay())
                     manager.getBot().closeAudioConnection(guildId);
-                // unpause, in the case when the player was paused and the track has been skipped.
-                // this is to prevent the player being paused next time it's being used.
+                // 取消暫停，以防播放器被暫停並且曲目已被跳過。
+                // 這是為了防止下次使用時播放器處於暫停狀態。
                 player.setPaused(false);
             }
         }
@@ -203,7 +201,7 @@ public class AudioHandler extends AudioEventAdapter implements AudioSendHandler
 
     @Override
     public void onTrackException(AudioPlayer player, AudioTrack track, FriendlyException exception) {
-        LoggerFactory.getLogger("AudioHandler").error("Track " + track.getIdentifier() + " has failed to play", exception);
+        LoggerFactory.getLogger("AudioHandler").error("曲目 " + track.getIdentifier() + " 播放失敗", exception);
     }
 
     @Override
@@ -214,7 +212,7 @@ public class AudioHandler extends AudioEventAdapter implements AudioSendHandler
     }
 
     
-    // Formatting
+    // 格式化
     public Message getNowPlaying(JDA jda)
     {
         if(isMusicPlaying(jda))
@@ -222,14 +220,14 @@ public class AudioHandler extends AudioEventAdapter implements AudioSendHandler
             Guild guild = guild(jda);
             AudioTrack track = audioPlayer.getPlayingTrack();
             MessageBuilder mb = new MessageBuilder();
-            mb.append(FormatUtil.filter(manager.getBot().getConfig().getSuccess()+" **Now Playing in "+guild.getSelfMember().getVoiceState().getChannel().getAsMention()+"...**"));
+            mb.append(FormatUtil.filter(manager.getBot().getConfig().getSuccess()+" **現在正在播放 "+guild.getSelfMember().getVoiceState().getChannel().getAsMention()+"...**"));
             EmbedBuilder eb = new EmbedBuilder();
             eb.setColor(guild.getSelfMember().getColor());
             RequestMetadata rm = getRequestMetadata();
             if(rm.getOwner() != 0L)
             {
                 User u = guild.getJDA().getUserById(rm.user.id);
-                if(u==null)
+                if(u == null)
                     eb.setAuthor(FormatUtil.formatUsername(rm.user), null, rm.user.avatar);
                 else
                     eb.setAuthor(FormatUtil.formatUsername(u), null, u.getEffectiveAvatarUrl());
@@ -250,7 +248,7 @@ public class AudioHandler extends AudioEventAdapter implements AudioSendHandler
             }
             
             if(track.getInfo().author != null && !track.getInfo().author.isEmpty())
-                eb.setFooter("Source: " + track.getInfo().author, null);
+                eb.setFooter("來源: " + track.getInfo().author, null);
 
             double progress = (double)audioPlayer.getPlayingTrack().getPosition()/track.getDuration();
             eb.setDescription(getStatusEmoji()
@@ -267,9 +265,9 @@ public class AudioHandler extends AudioEventAdapter implements AudioSendHandler
     {
         Guild guild = guild(jda);
         return new MessageBuilder()
-                .setContent(FormatUtil.filter(manager.getBot().getConfig().getSuccess()+" **Now Playing...**"))
+                .setContent(FormatUtil.filter(manager.getBot().getConfig().getSuccess()+" **現在正在播放...**"))
                 .setEmbeds(new EmbedBuilder()
-                .setTitle("No music playing")
+                .setTitle("沒有音樂正在播放")
                 .setDescription(STOP_EMOJI+" "+FormatUtil.progressBar(-1)+" "+FormatUtil.volumeIcon(audioPlayer.getVolume()))
                 .setColor(guild.getSelfMember().getColor())
                 .build()).build();
@@ -280,7 +278,7 @@ public class AudioHandler extends AudioEventAdapter implements AudioSendHandler
         return audioPlayer.isPaused() ? PAUSE_EMOJI : PLAY_EMOJI;
     }
     
-    // Audio Send Handler methods
+    // 音頻發送處理程序方法
     /*@Override
     public boolean canProvide() 
     {
@@ -322,7 +320,7 @@ public class AudioHandler extends AudioEventAdapter implements AudioSendHandler
     }
     
     
-    // Private methods
+    // 私有方法
     private Guild guild(JDA jda)
     {
         return jda.getGuildById(guildId);
