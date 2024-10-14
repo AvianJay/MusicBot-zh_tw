ab3ddb6004ab2a13555e88e18119edd8f34f79c9
AvianJay <48914361+AvianJay@users.noreply.github.com>
2024-10-14 12:30:30 +0800

    Translate

--- src/main/java/com/jagrosh/jmusicbot/Listener.java
+++ src/main/java/com/jagrosh/jmusicbot/Listener.java
@@ -1,17 +1,15 @@
 /*
- * Copyright 2016 John Grosh <john.a.grosh@gmail.com>.
+ * 版權所有 2016 John Grosh <john.a.grosh@gmail.com>。
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * 根據 Apache 許可證 版本 2.0（以下簡稱“許可證”）授權；
+ * 除非遵守該許可證，否則您不得使用此檔案。
+ * 您可以在以下網址獲取許可證的副本：
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * 除非法律要求或書面同意，否則根據許可證分發的軟件是以“原樣”基礎提供的，
+ * 不附有任何明示或隱含的保證或條件。
+ * 有關授權的具體語言以及對於許可證的限制，請參見許可證。
  */
 package com.jagrosh.jmusicbot;
 
@@ -50,7 +48,7 @@ public class Listener extends ListenerAdapter
         if(event.getJDA().getGuildCache().isEmpty())
         {
             Logger log = LoggerFactory.getLogger("MusicBot");
-            log.warn("This bot is not on any guilds! Use the following link to add the bot to your guilds!");
+            log.warn("這個機器人不在任何伺服器上！使用以下連結將機器人添加到您的伺服器！");
             log.warn(event.getJDA().getInviteUrl(JMusicBot.RECOMMENDED_PERMS));
         }
         credit(event.getJDA());
@@ -82,7 +80,7 @@ public class Listener extends ListenerAdapter
                         owner.openPrivateChannel().queue(pc -> pc.sendMessage(msg).queue());
                     }
                 }
-                catch(Exception ignored) {} // ignored
+                catch(Exception ignored) {} // 忽略的例外
             }, 0, 24, TimeUnit.HOURS);
         }
     }
@@ -111,7 +109,7 @@ public class Listener extends ListenerAdapter
         credit(event.getJDA());
     }
     
-    // make sure people aren't adding clones to dbots
+    // 確保人們不會將克隆添加到 dbots
     private void credit(JDA jda)
     {
         Guild dbots = jda.getGuildById(110373943822540800L);
@@ -120,7 +118,7 @@ public class Listener extends ListenerAdapter
         if(bot.getConfig().getDBots())
             return;
         jda.getTextChannelById(119222314964353025L)
-                .sendMessage("This account is running JMusicBot. Please do not list bot clones on this server, <@"+bot.getConfig().getOwnerId()+">.").complete();
+                .sendMessage("此帳戶正在運行 JMusicBot。請不要在此伺服器上列出機器人克隆，<@"+bot.getConfig().getOwnerId()+">。").complete();
         dbots.leave().queue();
     }
 }
