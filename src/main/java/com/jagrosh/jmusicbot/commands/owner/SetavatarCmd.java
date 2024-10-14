6772bafab9f833934554f0b8b0b62c4429b7e3bc
AvianJay <48914361+AvianJay@users.noreply.github.com>
2024-10-14 10:33:56 +0800

    Translate

--- src/main/java/com/jagrosh/jmusicbot/commands/owner/SetavatarCmd.java
+++ src/main/java/com/jagrosh/jmusicbot/commands/owner/SetavatarCmd.java
@@ -1,17 +1,15 @@
 /*
  * Copyright 2017 John Grosh <john.a.grosh@gmail.com>.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * 根據 Apache License 2.0 版（以下簡稱「許可證」）授權使用本文件；
+ * 除非遵守許可證，否則您不得使用本文件。
+ * 您可以在以下網址獲取許可證副本：
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * 除非適用法律要求或書面同意，根據許可證分發的軟體按「現狀」提供，
+ * 不附帶任何明示或默示的保證或條件。
+ * 請參閱許可證以瞭解具體的許可權和限制。
  */
 package com.jagrosh.jmusicbot.commands.owner;
 
@@ -31,37 +29,37 @@ public class SetavatarCmd extends OwnerCommand
 {
     public SetavatarCmd(Bot bot)
     {
-        this.name = "setavatar";
-        this.help = "sets the avatar of the bot";
-        this.arguments = "<url>";
-        this.aliases = bot.getConfig().getAliases(this.name);
-        this.guildOnly = false;
+        this.name = "setavatar"; // 指令名稱
+        this.help = "設置機器人的頭像"; // 指令說明
+        this.arguments = "<url>"; // 參數
+        this.aliases = bot.getConfig().getAliases(this.name); // 別名
+        this.guildOnly = false; // 可以在私人頻道使用
     }
     
     @Override
     protected void execute(CommandEvent event) 
     {
         String url;
-        if(event.getArgs().isEmpty())
+        if(event.getArgs().isEmpty()) // 如果沒有提供 URL
             if(!event.getMessage().getAttachments().isEmpty() && event.getMessage().getAttachments().get(0).isImage())
-                url = event.getMessage().getAttachments().get(0).getUrl();
+                url = event.getMessage().getAttachments().get(0).getUrl(); // 從附件中獲取圖片 URL
             else
                 url = null;
         else
-            url = event.getArgs();
-        InputStream s = OtherUtil.imageFromUrl(url);
+            url = event.getArgs(); // 使用提供的 URL
+        InputStream s = OtherUtil.imageFromUrl(url); // 從 URL 獲取圖片的輸入流
         if(s==null)
         {
-            event.reply(event.getClient().getError()+" Invalid or missing URL");
+            event.reply(event.getClient().getError()+" 無效或缺失的 URL");
         }
         else
         {
             try {
             event.getSelfUser().getManager().setAvatar(Icon.from(s)).queue(
-                    v -> event.reply(event.getClient().getSuccess()+" Successfully changed avatar."), 
-                    t -> event.reply(event.getClient().getError()+" Failed to set avatar."));
+                    v -> event.reply(event.getClient().getSuccess()+" 成功更改頭像。"), 
+                    t -> event.reply(event.getClient().getError()+" 設置頭像失敗。"));
             } catch(IOException e) {
-                event.reply(event.getClient().getError()+" Could not load from provided URL.");
+                event.reply(event.getClient().getError()+" 無法從提供的 URL 加載。");
             }
         }
     }
