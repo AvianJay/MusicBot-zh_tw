75f7be4f2d090efcdd4231f30d9ccf5b475d0ec0
AvianJay <48914361+AvianJay@users.noreply.github.com>
2024-10-14 10:08:36 +0800

    Translate

--- src/main/java/com/jagrosh/jmusicbot/commands/music/SCSearchCmd.java
+++ src/main/java/com/jagrosh/jmusicbot/commands/music/SCSearchCmd.java
@@ -1,17 +1,15 @@
 /*
- * Copyright 2016 John Grosh <john.a.grosh@gmail.com>.
+ * 版權所有 2016 John Grosh <john.a.grosh@gmail.com>.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * 根據 Apache License 2.0 版（以下簡稱「許可證」）授權使用本文件；
+ * 除非遵守許可證，否則您不得使用本文件。
+ * 您可以在以下網址獲取許可證副本：
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * 除非適用法律要求或書面同意，根據許可證分發的軟體按「現狀」提供，
+ * 不附帶任何明示或默示的保證或條件。
+ * 請參閱許可證以瞭解具體的許可權和限制。
  */
 package com.jagrosh.jmusicbot.commands.music;
 
@@ -26,9 +24,9 @@ public class SCSearchCmd extends SearchCmd
     public SCSearchCmd(Bot bot)
     {
         super(bot);
-        this.searchPrefix = "scsearch:";
-        this.name = "scsearch";
-        this.help = "searches Soundcloud for a provided query";
-        this.aliases = bot.getConfig().getAliases(this.name);
+        this.searchPrefix = "scsearch:"; // 使用 Soundcloud 搜索的前綴
+        this.name = "scsearch"; // 指令名稱為 scsearch
+        this.help = "搜尋 Soundcloud 上提供的查詢"; // 指令說明
+        this.aliases = bot.getConfig().getAliases(this.name); // 指令的別名
     }
 }
