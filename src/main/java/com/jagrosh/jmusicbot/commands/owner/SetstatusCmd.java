a0cf3e8bcb5a5f99a1355115c5bb9ac3a10f73e9
AvianJay <48914361+AvianJay@users.noreply.github.com>
2024-10-14 10:38:46 +0800

    Translate

--- src/main/java/com/jagrosh/jmusicbot/commands/owner/SetstatusCmd.java
+++ src/main/java/com/jagrosh/jmusicbot/commands/owner/SetstatusCmd.java
@@ -1,17 +1,15 @@
 /*
  * Copyright 2017 John Grosh <john.a.grosh@gmail.com>.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * 根據 Apache License 2.0 版（以下簡稱「許可證」）授權使用本文件；
+ * 除非遵守許可證，否則您不得使用本文件。
+ * 您可以在以下網址獲取許可證副本：
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * 除非適用法律要求或書面同意，根據許可證分發的軟體按「現狀」提供，
+ * 不附帶任何明示或默示的保證或條件。
+ * 請參閱許可證以瞭解具體的許可權和限制。
  */
 package com.jagrosh.jmusicbot.commands.owner;
 
@@ -28,29 +26,29 @@ public class SetstatusCmd extends OwnerCommand
 {
     public SetstatusCmd(Bot bot)
     {
-        this.name = "setstatus";
-        this.help = "sets the status the bot displays";
-        this.arguments = "<status>";
-        this.aliases = bot.getConfig().getAliases(this.name);
-        this.guildOnly = false;
+        this.name = "setstatus"; // 指令名稱
+        this.help = "設置機器人顯示的狀態"; // 指令說明
+        this.arguments = "<status>"; // 參數
+        this.aliases = bot.getConfig().getAliases(this.name); // 別名
+        this.guildOnly = false; // 可以在私人頻道使用
     }
     
     @Override
     protected void execute(CommandEvent event) 
     {
         try {
-            OnlineStatus status = OnlineStatus.fromKey(event.getArgs());
-            if(status==OnlineStatus.UNKNOWN)
+            OnlineStatus status = OnlineStatus.fromKey(event.getArgs()); // 根據用戶輸入的狀態獲取狀態
+            if(status == OnlineStatus.UNKNOWN) // 如果狀態無效
             {
-                event.replyError("Please include one of the following statuses: `ONLINE`, `IDLE`, `DND`, `INVISIBLE`");
+                event.replyError("請包括以下狀態之一：`ONLINE`、`IDLE`、`DND`、`INVISIBLE`");
             }
             else
             {
-                event.getJDA().getPresence().setStatus(status);
-                event.replySuccess("Set the status to `"+status.getKey().toUpperCase()+"`");
+                event.getJDA().getPresence().setStatus(status); // 設置狀態
+                event.replySuccess("狀態已設置為 `"+status.getKey().toUpperCase()+"`");
             }
         } catch(Exception e) {
-            event.reply(event.getClient().getError()+" The status could not be set!");
+            event.reply(event.getClient().getError()+" 無法設置狀態！");
         }
     }
 }
